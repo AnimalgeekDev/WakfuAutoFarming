d7b0c7d1c9efa1c30602d7181c82023ce7b54a67
Animalgeek <animalgeek.dev@gmail.com>
2026-02-21 18:41:12 +0100

    Update requirements and enhance YOLO training script with TensorBoard logging

--- scripts/run_yolo.py
+++ scripts/run_yolo.py
@@ -19,6 +19,6 @@ def run_yolo_model(model_path, image_path):
 
 if __name__ == "__main__":
     # Example usage
-    trained_model_path = "runs/detect/train/weights/best.pt"  # Replace with the actual path to your trained model
+    trained_model_path = "runs/detect/yolov8s/weights/best.pt"  # Replace with the actual path to your trained model
     test_image_path = "train_data/test/images/Captura-de-pantalla-16-_png.rf.ab008854e0449d7133e0a1ef9158e3cf.jpg"  # Replace with the actual path to your test image
     run_yolo_model(model_path=trained_model_path, image_path=test_image_path)
--- scripts/train_yolo.py
+++ scripts/train_yolo.py
@@ -1,6 +1,7 @@
 from ultralytics import YOLO
 
-def train_yolo_model(data_path, model_type='yolov8n', epochs=10):
+
+def train_yolo_model(data_path, model_type, epochs, project='wakfu', name='train_logs'):
     """
     Train a YOLO model.
 
@@ -8,14 +9,35 @@ def train_yolo_model(data_path, model_type='yolov8n', epochs=10):
         data_path (str): Path to the dataset YAML file.
         model_type (str): Type of YOLO model to use (e.g., 'yolov8n', 'yolov8s').
         epochs (int): Number of training epochs.
+        project (str): Directory to save training results and logs.
+        name (str): Name of the training run.
     """
     # Initialize the YOLO model
     model = YOLO(model_type)
 
-    # Train the model
-    model.train(data=data_path, epochs=epochs)
+    # Train the model with TensorBoard logging enabled
+    model.train(data=data_path, epochs=epochs, project=project, name=name)
+
 
 if __name__ == "__main__":
-    # Example usage
-    dataset_path = "train_data/data.yaml"  # Replace with the actual path to your dataset YAML file
-    train_yolo_model(data_path=dataset_path, model_type='yolov8n', epochs=100)
+    # Define training configurations
+    training_configs = [
+        # {"dataset_path": "train_data/WakfuFarming.v3i.yolov8/data.yaml", "models": [
+        #     {"model_name": "yolov8n", "epochs": [10, 25, 50, 75]},
+        #     {"model_name": "yolov8s", "epochs": [10, 25, 50, 75]}
+        # ]},
+        {"dataset_path": "train_data/WakfuFarming.v3i.yolo26/data.yaml", "models": [
+            {"model_name": "yolo26n", "epochs": [10, 25, 50, 75]},
+            {"model_name": "yolo26s", "epochs": [10, 25, 50, 75]}
+        ]}
+    ]
+
+    # Train models based on configurations
+    for config in training_configs:
+        dataset_path = config["dataset_path"]
+        for model_config in config["models"]:
+            model_name = model_config["model_name"]
+            for epoch in model_config["epochs"]:
+                folder_name = f"{model_name}-{epoch}"
+                train_yolo_model(
+                    data_path=dataset_path, model_type=model_name, epochs=epoch, name=folder_name)
